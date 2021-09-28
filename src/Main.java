dab01ba5b49ccf5bc3a00ca33464c66797506ed0
Sławomir Majchrzak <smmaya@yahoo.fr>
2021-09-28 10:29:38 +0200

    ćwiczenie 2

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,12 @@ public class Main {
         /* zad.1 Napisz program który będzie iterował od 100 do 0,
          *  i wyświetlał liczby które są podzielne przez ostatnią cyfrę waszego indeksu
          * (jesli tą liczbą jest 0 to przez 2 ostatnie) */
-
+        int b = 3;
+        for (int i = 100; i >= 0; i--) {
+            if (i % 3 == 0 && i !=0) {
+                System.out.println( i + "/" + b + " = " + i/b);
+            }
+        }
 
         /* zad.2 Napisz program który będzie uzupełniał tablicę 5-cio elementową int[] liczbami
          * wprowadzanymi z klawiatury (użyć klasy Scanner), a następnie będzie wyświetlał elementy tablicy
