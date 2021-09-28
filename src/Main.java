f432464cfdea5d824e7430a53a99f2eccecb1d0f
Sławomir Majchrzak <smmaya@yahoo.fr>
2021-09-28 11:07:25 +0200

    ćwiczenie 2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -13,10 +16,29 @@ public class Main {
                 System.out.println( i + "/" + b + " = " + i/b);
             }
         }
+        System.out.println("");
 
         /* zad.2 Napisz program który będzie uzupełniał tablicę 5-cio elementową int[] liczbami
          * wprowadzanymi z klawiatury (użyć klasy Scanner), a następnie będzie wyświetlał elementy tablicy
          * za pomocą pętli forEach powiększone o 11 */
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ile chcesz elementów tablicy: ");
+        //czytamy ilość chcianych elementów
+        n = sc.nextInt();
+        //tworzymy tablicę z 10 elementami
+        int[] array = new int[10];
+        System.out.println("Wprowadź liczby całkowite: ");
+        for(int i=0; i<n; i++)
+        {
+        //czytamy elementy tablicy wpisane przez użytkownika
+            array[i] = sc.nextInt();
+        }
+        System.out.println("Lista elementów: ");
+        // odczytujemy elementy pętlą i do każdego elementu dodajemy 11
+        for (int i=0; i<n; i++) {
+            System.out.println(array[i]+11);
+        }
 
     }
 }
