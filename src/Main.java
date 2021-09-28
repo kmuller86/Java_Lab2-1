54dc58ae37d9cfa3632d85091beb095e6a05f619
Sławomir Majchrzak <smmaya@yahoo.fr>
2021-09-28 13:12:42 +0200

    ćwiczenie 3

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,28 @@ public class Main {
           a) ta liczba jest parzysta --> podpowiedz switch(a % 2) {
           b) program który określi czy liczba jest podzielna przez 7
          */
+        // Do wyłapania inputu użytkownika
+        int a;
+        // Tworzenie skanera do danych wejściowych.
+        Scanner console = new Scanner(System.in);
+        // Zapytanie o podanie liczby.
+        System.out.print("Wpisz dowolną liczbę: ");
+        a = console.nextInt();
+        // Określić co użytkownik wpisał.
+        switch (a % 2)
+        {
+            case 0 :
+                System.out.println("Liczba jest parzysta.");
+                break;
+            case 1 :
+                System.out.println("Liczba jest nieparzysta.");
+                break;
+        }
+        if(a % 7 == 0){
+            System.out.println("Liczba " + a + " jest podzielna przez 7." + "\nWynik dzielenia to: " + a / 7);
+        } else {
+            System.out.println("Liczba " + a + " nie dzieli się przez 7.");
+        }
 
         /* zad.2
            a) napisać program podobny do programu z przykładu, który ma zliczyć iloś liter 'o', 'p', 'x', 'z', 'q' w wyrazie:
