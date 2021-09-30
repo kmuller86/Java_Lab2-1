6a4e870cde91b437b64c2d22ae5a1bddf51aa000
Sławomir Majchrzak <smmaya@yahoo.fr>
2021-09-30 14:47:51 +0200

    ćwiczenie 3

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.OutputStream;
 import java.util.Scanner;
 
 public class Main {
@@ -33,12 +34,40 @@ public class Main {
         } else {
             System.out.println("Liczba " + a + " nie dzieli się przez 7.");
         }
-
+        System.out.println();
         /* zad.2
            a) napisać program podobny do programu z przykładu, który ma zliczyć iloś liter 'o', 'p', 'x', 'z', 'q' w wyrazie:
             "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo"
            b) oraz ma określić ile jest pozostałych liter, które nie należą do: 'o', 'p', 'x', 'z', 'q'
          */
-
+        int o_count = 0, p_count = 0, x_count = 0, z_count = 0, q_count = 0, otherLetters_count = 0;
+        String wyraz = "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo";
+        for(char znak : wyraz.toCharArray()){
+            if(znak == 'o'){
+                o_count++;
+            }
+            else if(znak == 'p'){
+                p_count++;
+            }
+            else if(znak == 'x'){
+                x_count++;
+            }
+            else if(znak == 'z'){
+                z_count++;
+            }
+            else if(znak == 'q'){
+                q_count++;
+            }
+            else{
+                otherLetters_count++;
+            }
+        }
+        System.out.println("Ilość poszczególnych liter w wyrazie:\n" + wyraz);
+        System.out.println("o: " + o_count);
+        System.out.println("p: " + p_count);
+        System.out.println("x: " + x_count);
+        System.out.println("z: " + z_count);
+        System.out.println("q: " + q_count);
+        System.out.println("Pozostałe litery: " + otherLetters_count + " razy");
     }
 }
