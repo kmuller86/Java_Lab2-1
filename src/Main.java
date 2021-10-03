f833e1c801a24906e9575076f842716c308f2370
Sławomir Majchrzak <smmaya@yahoo.fr>
2021-10-03 15:05:29 +0200

    ćwiczenie 6

--- src/Main.java
+++ src/Main.java
@@ -8,21 +8,32 @@ public class Main {
 
         /* ćw_6 Listy */
 
-        /* zad.1 a) Napisać program który stworzy listę ArrayList<String> (lista tablicowa),
-                Proszę dane wprowadzać z klawiatury (można użyć pętli), lista ma mieć przynajmniej 5
-                elementów i elementami mają być nazwy zwierząt.
-                b) proszę stworzyć metodę która będzie miała pętle forEach i będzie wyświetlała elementy Listy
-                -- podpowiedź: for(String zmienna : List<String>)
-                c) proszę usunąć 2 ostatnie elementy listy i dodać do niej 3 inne elementy,
-                następnie wyświetlić listę i wielkość listy,
-                d) następnie proszę wykonać odwrotne sortowanie i znów użyć metody z punktu b) do wyświetlenia
-                elementów listy
+        /* zad.1
+        a) Napisać program który stworzy listę ArrayList<String> (lista tablicowa),
+           Proszę dane wprowadzać z klawiatury (można użyć pętli), lista ma mieć przynajmniej 5
+           elementów i elementami mają być nazwy zwierząt.
+        b) proszę stworzyć metodę która będzie miała pętle forEach i będzie wyświetlała elementy Listy
+           -- podpowiedź: for(String zmienna : List<String>)
+        c) proszę usunąć 2 ostatnie elementy listy i dodać do niej 3 inne elementy,
+           następnie wyświetlić listę i wielkość listy,
+        d) następnie proszę wykonać odwrotne sortowanie i znów użyć metody z punktu b) do wyświetlenia
+           elementów listy
           */
+        Set<String> zwierzaki = new TreeSet<>();
+        Scanner scan = new Scanner((System.in));
+        System.out.print("Wypisz kilka nazw zwierząt, choć jedn musi się powtórzyć : ");
+        int max = 5;
+        for (int i = 0; i < max; i++) {
+            zwierzaki.add(scan.nextLine());
+        }
 
+        System.out.println("zwierzaki: " + zwierzaki);
+        System.out.println(zwierzaki.size());
 
-         /* zad.2 Napisać program który stworzy Set<Integer> i wprowadzamy 10 liczb z klawiatury
-                niech liczby się powtarzają, proszę posortować a następnie wyświetlić zawartość set-a w konsoli
-                 a następnie przeiterować i wyświetlić forEach-em (jak wyżej)
+         /* zad.2
+         Napisać program który stworzy Set<Integer> i wprowadzamy 10 liczb z klawiatury
+         niech liczby się powtarzają, proszę posortować a następnie wyświetlić zawartość set-a w konsoli
+         a następnie przeiterować i wyświetlić forEach-em (jak wyżej)
          */
 
 
