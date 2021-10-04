8cd29bd529d9a993bc06d34f6c902632bf89d9ee
Sławomir Majchrzak <smmaya@yahoo.fr>
2021-10-04 09:05:19 +0200

    ćwiczenie 6

--- src/Main.java
+++ src/Main.java
@@ -19,24 +19,58 @@ public class Main {
         d) następnie proszę wykonać odwrotne sortowanie i znów użyć metody z punktu b) do wyświetlenia
            elementów listy
           */
-        Set<String> zwierzaki = new TreeSet<>();
+        List<String> zwierzaki = new ArrayList<>();
         Scanner scan = new Scanner((System.in));
-        System.out.print("Wypisz kilka nazw zwierząt, choć jedn musi się powtórzyć : ");
+        System.out.print("Wypisz kilka nazw zwierząt : ");
+
         int max = 5;
         for (int i = 0; i < max; i++) {
             zwierzaki.add(scan.nextLine());
         }
-
         System.out.println("zwierzaki: " + zwierzaki);
-        System.out.println(zwierzaki.size());
+
+        for(String x : zwierzaki) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+
+        int zdjeto = 0; // Setup the variable for removal counting
+        while (zdjeto < Math.min(zwierzaki.size(), 2)) { // While we still haven't removed 2 entries OR second list size
+            zwierzaki.remove(zwierzaki.size() - 1); // Remove the last entry of the list
+            zdjeto++; // Increases 'zdjeto' count
+        }
+        zwierzaki.add("Słoń");
+        zwierzaki.add("Nosorożec");
+        zwierzaki.add("Hippopotam");
+        System.out.println("Zmodyfikowane zwierzaki: " + zwierzaki);
+
+        //Collections.reverse(zwierzaki);
+/*        for(String x : zwierzaki) {
+            System.out.print(x + " ");
+        }*/
+        System.out.println("Twoje zwierzaki od tyłu: ");
+        for(int a = zwierzaki.size() - 1; a >= 0; a--)
+        {
+            System.out.print(zwierzaki.get(a) + " ");
+        }
+
+        System.out.println();
+        //System.out.println("zwierzaki od tyłu jako lista: " + zwierzaki);
+        System.out.println("Rozmiar listy: " + zwierzaki.size());
 
          /* zad.2
          Napisać program który stworzy Set<Integer> i wprowadzamy 10 liczb z klawiatury
          niech liczby się powtarzają, proszę posortować a następnie wyświetlić zawartość set-a w konsoli
          a następnie przeiterować i wyświetlić forEach-em (jak wyżej)
          */
+            Set<String> liczby = new TreeSet<>();
+            Scanner scan2 = new Scanner((System.in));
 
-
+            System.out.print("Wypisz kilka liczb, niech się niektórw powtórzą : ");
+            int max2 = 10;
+            for (int i = 0; i < max2; i++) {
+                liczby.add(scan2.nextLine());
+            }
+            System.out.println("Twoje Liczby to: " + liczby + " bez duplikatów i posortowane rosnąco.");
     }
-
 }
